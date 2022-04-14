96eb9f2f1de9d6f7d4a52739a07ac32c34f0de55
Tanner Patterson <phoenixgroup2b2t@gmail.com>
2022-04-14 09:42:38 -0500

    minor fix and target classes

--- src/main/java/dmacc/controller/RentalCarController.java
+++ src/main/java/dmacc/controller/RentalCarController.java
@@ -1,7 +1,6 @@
 package dmacc.controller;
 
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
