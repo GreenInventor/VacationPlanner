8c71d285ee3bc0594f626ba728ef1b0df50cd068
Chris Pohlman <c.pohlmanmx+github@gmail.com>
2022-04-14 09:22:03 -0500

    planner controller updated to include insert and update functions

--- src/main/java/dmacc/controller/PlannerController.java
+++ src/main/java/dmacc/controller/PlannerController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import dmacc.beans.Planner;
 import dmacc.repository.PlannerRepository;
 
 /**
@@ -24,6 +28,33 @@ public class PlannerController {
 		return "plannerviewer";
 	}
 	
+	@GetMapping("/inputPlanner")
+	public String addNewPlan(Model model) {
+		Planner p = new Planner();
+		model.addAttribute("newPlanner",p);
+		return "planner";
+	}
+	
+	@PostMapping("/inputPlanner")
+	public String addNewPlan(@ModelAttribute Planner p, Model model) {
+		repo.save(p);
+		System.out.println("Planner Created");
+		System.out.println(p.toString());
+		return viewAllPlans(model);
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String updatePlanner(@PathVariable("id") long id, Model model) {
+		Planner p = repo.findById(id).orElse(null);
+		model.addAttribute("newPlanner", p);
+		return "planner";
+	}
+	
+	@PostMapping("/update/{id}")
+	public String revisePlanner(Planner p, Model model) {
+		repo.save(p);
+		return viewAllPlans(model);
+	}
 	
 
 }
