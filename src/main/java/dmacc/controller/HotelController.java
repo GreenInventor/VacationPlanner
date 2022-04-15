275b47878f95878104179ab5388a140754d0b691
colbywemer <78008250+colbywemer@users.noreply.github.com>
2022-04-15 17:01:18 -0500

    Added ability to filter hotels by state

--- src/main/java/dmacc/controller/HotelController.java
+++ src/main/java/dmacc/controller/HotelController.java
@@ -1,11 +1,14 @@
 package dmacc.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import dmacc.beans.Hotel;
 import dmacc.repository.HotelRepository;
@@ -36,6 +39,12 @@ public class HotelController {
 			return addNewHotel(model);
 		}
 		model.addAttribute("hotels", repo.findAll());
+		return "viewAllHotels";
+	}
+	@PostMapping("/findHotelByState")
+	public String findHotelByState(Model model, @RequestParam(name="state") String state) {
+		List<Hotel> h = repo.findByHotelAddressState(state);
+		model.addAttribute("hotels", h);
 		return "viewAllHotels";
 	}
 }
--- src/main/java/dmacc/repository/HotelRepository.java
+++ src/main/java/dmacc/repository/HotelRepository.java
@@ -1,5 +1,7 @@
 package dmacc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import dmacc.beans.Hotel;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long>{
-
+	public List<Hotel> findByHotelAddressState(String state);
 }
