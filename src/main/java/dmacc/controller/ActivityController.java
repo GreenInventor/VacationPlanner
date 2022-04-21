6b66644bea1a53edee83b08b4cb673b684fc1236
callbee20 <callbee@dmacc.edu>
2022-04-21 00:08:39 -0500

    Just added a very simple webpage to add activities

--- src/main/java/dmacc/controller/ActivityController.java
+++ src/main/java/dmacc/controller/ActivityController.java
@@ -1,5 +1,12 @@
 package dmacc.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import dmacc.beans.Activities;
+
 /**
  * @author Carson Allbee callbee
  *CIS175 - Fall 2021
@@ -7,4 +14,18 @@ package dmacc.controller;
  */
 public class ActivityController {
 
+	@GetMapping("/inputActivity")
+	public String addNewActivity(Model model) {
+		Activities a = new Activities();
+		model.addAttribute("newActivity", a);
+		return "input";
+	}
+	
+	//@PostMapping("/inputActivity")
+	//public String addNewActivity(@ModelAttribute Activities a, Model model) {
+		//repo.save(a);
+
+	//}
+	
+	
 }
