a1700a6ff38f7a37e5894b42395dea185106eb73
colbywemer <78008250+colbywemer@users.noreply.github.com>
2022-04-28 04:05:11 -0500

    Added Input Validation To Event And Activity

--- src/main/java/dmacc/beans/Restaurant.java
+++ src/main/java/dmacc/beans/Restaurant.java
@@ -1,69 +1,23 @@
 package dmacc.beans;
 
-/**
- * @author Carson Allbee callbee
- *CIS175 - Fall 2021
- * Apr 27, 2022
- */
-public class Restaurant {
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Restaurant {
+	@Id
+	@GeneratedValue
 	private long id;
 	private String name;
-	private String address;
-	
-	public Restaurant() {
-		super();
-	}
-	
-	public Restaurant(long id, String name, String address) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.address = address;
-	}
-	
-	public Restaurant(String name, String address) {
-		super();
-		this.name = name;
-		this.address = address;
-	}
-	
-	/**
-	 * @return the id
-	 */
-	public long getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(long id) {
-		this.id = id;
-	}
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	/**
-	 * @return the address
-	 */
-	public String getAddress() {
-		return address;
-	}
-	/**
-	 * @param address the address to set
-	 */
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
+	private Address address;
 	
 }
