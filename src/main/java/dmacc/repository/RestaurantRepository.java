c3418ad77d5978584d39e6a9f12df441a7379ae7
connorc37 <84937675+connorc37@users.noreply.github.com>
2022-04-28 12:39:51 -0500

    Added to the restaurant controller
    Be right back - uploading what I wrote earlier.

--- src/main/java/dmacc/repository/RestaurantRepository.java
+++ src/main/java/dmacc/repository/RestaurantRepository.java
@@ -4,11 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import dmacc.beans.Restaurant;
 
-/**
- * @author Carson Allbee callbee
- *CIS175 - Fall 2021
- * Apr 27, 2022
- */
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long>{
 
 }
